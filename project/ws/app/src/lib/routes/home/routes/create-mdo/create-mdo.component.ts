bc65a731bac3dda091368e66b7bb033197189cf4
amantarento <amankumar.sharma@tarento.com>
2021-09-29 12:53:02 +0530

    fix the issue of mdo portal of user creation and department name

--- project/ws/app/src/lib/routes/home/routes/create-mdo/create-mdo.component.ts
+++ project/ws/app/src/lib/routes/home/routes/create-mdo/create-mdo.component.ts
@@ -71,12 +71,12 @@ export class CreateMdoComponent implements OnInit {
   { isActive: false, isCompleted: false, name: 'Classification', step: 1 },
   { isActive: false, isCompleded: false, name: 'Intended for', step: 2 }]
   constructor(public dialog: MatDialog,
-    private snackBar: MatSnackBar,
-    private createMdoService: CreateMDOService,
-    private router: Router,
-    private directoryService: DirectoryService,
-    private valueSvc: ValueService,
-    private activatedRoute: ActivatedRoute) {
+              private snackBar: MatSnackBar,
+              private createMdoService: CreateMDOService,
+              private router: Router,
+              private directoryService: DirectoryService,
+              private valueSvc: ValueService,
+              private activatedRoute: ActivatedRoute) {
     {
 
       this.loggedInUserId = _.get(this.activatedRoute, 'snapshot.parent.data.configService.userProfile.userId')
@@ -189,7 +189,7 @@ export class CreateMdoComponent implements OnInit {
             this.snackBar.open('Admin assigned Successfully')
             this.router.navigate(['/app/home/directory', { department: this.department }])
           }
-        }, (err: { error: any }) => {
+        },                                                                                                              (err: { error: any }) => {
           this.openSnackbar(err.error.errors[0].message)
         })
       })
@@ -245,7 +245,7 @@ export class CreateMdoComponent implements OnInit {
       if (this.contentForm.value.name !== null
         && this.contentForm.value.deptSubTypeId !== null) {
         this.createMdoService.createDepartment(this.contentForm.value, this.deptType,
-          this.department, this.loggedInUserId).subscribe(res => {
+                                               this.department, this.loggedInUserId).subscribe(res => {
             if (res.result.response === 'SUCCESS') {
               this.submittedForm = false
               const obj = {
@@ -259,11 +259,10 @@ export class CreateMdoComponent implements OnInit {
           })
       }
     } else {
-      console.log(this.contentForm.value)
       if (this.contentForm.value.name !== null
         && this.contentForm.value.deptSubTypeId !== null) {
         this.createMdoService.updateDepartment(this.updateId, this.contentForm.value.deptSubTypeId,
-          this.department, this.loggedInUserId).subscribe(res => {
+                                               this.department, this.loggedInUserId).subscribe(res => {
             if (res.result.response === 'SUCCESS') {
               this.openSnackbar(`Success`)
               this.router.navigate([`/app/home/directory`])
